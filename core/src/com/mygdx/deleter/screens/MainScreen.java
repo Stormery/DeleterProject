9b3d224b6fcfa26aa6d767bc012487b6c703cbe9
Ayo-PC\Ayo <panstormery@gmail.com>
2017-07-28 20:16:31 +0100

    Table is ready

--- core/src/com/mygdx/deleter/screens/MainScreen.java
+++ core/src/com/mygdx/deleter/screens/MainScreen.java
@@ -4,14 +4,22 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.mygdx.deleter.DeleterProject;
 
 
 public class MainScreen extends AbstractScreen {
 
     SpriteBatch batch;
-    Image img;
     protected Stage stage;
+
+    Image imgBackground;
+
+    //Tables
+   private boolean tableDebug = true;
+    private Table tableMain;
+    private Table tableTop;
+
 
     public MainScreen(DeleterProject dp) {
         super();
@@ -22,8 +30,41 @@ public class MainScreen extends AbstractScreen {
     protected void init() {
         batch = new SpriteBatch();
         stage = new Stage();
-        img = new Image( new Texture("background.png"));
-        stage.addActor(img);
+        initBackground();
+         initTables();
+
+    }
+
+    private void initTables() {
+        tableMain = new Table();
+        tableMain.setFillParent(true);
+        tableMain.setDebug(true);
+        tableMain.top().left().padTop(10f);
+        stage.addActor(tableMain);
+
+        tableTop = new Table();
+        tableTop.setDebug(true);
+        tableTop.top();
+        ///TOP
+        tableTop.add().width(230f).height(145f).padLeft(10f);
+        tableTop.add().width(230f).height(145f).padLeft(45f);
+        tableTop.add().width(95f).height(145f).padLeft(65f);
+        tableTop.row();
+        //MID
+        tableTop.add().expandX().height(140f).colspan(3).padTop(35f);
+        tableTop.row();
+        //BOT
+        tableTop.add().width(510f).height(140f).colspan(2).padTop(28f);
+        tableTop.add().width(100f).height(55f).padTop(28f).padLeft(30f);
+        
+
+
+        tableMain.add(tableTop);
+    }
+
+    private void initBackground() {
+        imgBackground = new Image( new Texture("background.png"));
+        stage.addActor(imgBackground);
     }
 
 
